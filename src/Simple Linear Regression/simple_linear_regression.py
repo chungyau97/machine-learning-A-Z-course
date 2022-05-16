b3c34d6c7e96a161b944d04b3084f94bea5ac8c4
chungyau97 <chungyau97@gmail.com>
2022-05-16 21:14:35 +0800

    Add questions

--- src/Simple Linear Regression/simple_linear_regression.py
+++ src/Simple Linear Regression/simple_linear_regression.py
@@ -37,3 +37,18 @@ plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
+
+#Question 1: How do I use my simple linear regression model to make a single prediction, for example, to predict the salary of an employee with 12 years of experience?
+print(regressor.predict([[12]]))
+# Therefore, our model predicts that the salary of an employee with 12 years of experience is $ 138531.
+# Important note: Notice that the value of the feature (12 years) was input in a double pair of square brackets. That's because the "predict" method always expects a 2D array as the format of its inputs. And putting 12 into a double pair of square brackets makes the input exactly a 2D array. Simply put:
+# 12→scalar 
+# [12]→1D array 
+# [[12]]→2D array
+
+#Question 2: How do I get the final regression equation y = b0 + b1 x with the final values of the coefficients b0 and b1?
+print(regressor.coef_) #b0
+print(regressor.intercept_) #b1
+# Therefore, the equation of our simple linear regression model is:
+#           Salary=9312.57×YearsExperience+26780.09
+# Important Note: To get these coefficients we called the "coef_" and "intercept_" attributes from our regressor object. Attributes in Python are different than methods and usually return a simple value or an array of values.
