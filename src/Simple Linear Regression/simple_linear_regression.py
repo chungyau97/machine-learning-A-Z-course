bb93fbb580e27087b955a0ecd89ea4edfbc1db7f
chungyau97 <chungyau97@gmail.com>
2022-05-16 20:20:10 +0800

    Train simple linear regression model

--- src/Simple Linear Regression/simple_linear_regression.py
+++ src/Simple Linear Regression/simple_linear_regression.py
@@ -15,7 +15,9 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Training the Simple Linear Regression model on the Training set
-
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 
