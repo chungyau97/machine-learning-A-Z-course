fb303d618c6cefa0f733ccf22ac40599639a7045
chungyau97 <chungyau97@gmail.com>
2022-05-18 23:13:01 +0800

    Add method to smooth curve

--- src/03 Polynomial Regression/polynomial_regression.py
+++ src/03 Polynomial Regression/polynomial_regression.py
@@ -17,7 +17,7 @@ lin_reg.fit(X, y)
 
 # Training the Polynomial Regression model on the whole dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2) #Start from 2 
+poly_reg = PolynomialFeatures(degree = 4) #Start from 2 
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
@@ -39,6 +39,14 @@ plt.ylabel('Salary')
 plt.show()
 
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
 
 # Predicting a new result with Linear Regression
 
