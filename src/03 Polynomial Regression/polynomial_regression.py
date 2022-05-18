3107a36163c4c8d1f5a8a4a736d1a6d3acbc725a
chungyau97 <chungyau97@gmail.com>
2022-05-18 23:10:34 +0800

    Visualise linear regression and polynomial regression

--- src/03 Polynomial Regression/polynomial_regression.py
+++ src/03 Polynomial Regression/polynomial_regression.py
@@ -23,8 +23,20 @@ lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
 # Visualising the Linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
 # Visualising the Polynomial Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
 
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
 
