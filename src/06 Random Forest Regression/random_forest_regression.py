9c44b1981e01c6aa6ae869c43c3dc2336a08aff1
chungyau97 <chungyau97@gmail.com>
2022-05-22 23:27:08 +0800

    Add data preprocessing

---
+++ src/06 Random Forest Regression/random_forest_regression.py
@@ -0,0 +1,17 @@
+# Random Forest Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Training the Random Forest Regression model on the whole dataset
+
+# Predicting a new result
+
+# Visualising the Random Forest Regression results (higher resolution)
