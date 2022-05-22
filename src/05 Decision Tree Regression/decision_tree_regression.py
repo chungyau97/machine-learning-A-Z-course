dd6507a4735b378ee8b365ecd2af7120c927782b
chungyau97 <chungyau97@gmail.com>
2022-05-22 17:07:51 +0800

    Add data preprocessing

---
+++ src/05 Decision Tree Regression/decision_tree_regression.py
@@ -0,0 +1,17 @@
+# Decision Tree Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Training the Decision Tree Regression model on the whole dataset
+
+# Predicting a new result
+
+# Visualising the Decision Tree Regression results (higher resolution)
