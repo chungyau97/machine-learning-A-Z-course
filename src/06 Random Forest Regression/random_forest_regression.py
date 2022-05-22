cf822066eb9d822eaab92be13bd1cf426fe9a7d9
chungyau97 <chungyau97@gmail.com>
2022-05-22 23:29:48 +0800

    Train random forest regression model

--- src/06 Random Forest Regression/random_forest_regression.py
+++ src/06 Random Forest Regression/random_forest_regression.py
@@ -11,6 +11,9 @@ X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # Training the Random Forest Regression model on the whole dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X, y)
 
 # Predicting a new result
 
