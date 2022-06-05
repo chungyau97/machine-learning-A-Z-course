44c698adef7f2453023bb6ab4b94f98fc18cafe9
chungyau97 <chungyau97@gmail.com>
2022-06-05 22:49:18 +0800

    Add data preprocessing

---
+++ src/03 Classification/01 Logistic Regression/logistic_regression.py
@@ -0,0 +1,42 @@
+# Logistic Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, :] = sc.fit_transform(X_train)
+X_test[:, :] = sc.transform(X_test)
+print(X_train)
+print(X_test)
+
+# Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+
+# Predicting a new result
+
+# Predicting the Test set results
+
+# Making the Confusion Matrix
+
+# Visualising the Training set results
+
+# Visualising the Test set results
