2307cad1d2343232c99691f8f823c9c7ca7239ea
chungyau97 <chungyau97@gmail.com>
2022-06-26 16:55:23 +0800

    Preprocess data for KNN model

---
+++ src/03 Classification/02 - K-Nearest Neighbors (K-NN)/k_nearest_neighbors.py
@@ -0,0 +1,33 @@
+# K-Nearest Neighbors (K-NN)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit(X_train)
+X_test = sc.transform(X_test)
+
+# Training the K-NN model on the Training set
+
+# Predicting a new result
+
+# Predicting the Test set results
+
+# Making the Confusion Matrix
+
+# Visualising the Training set results
+
+# Visualising the Test set results
